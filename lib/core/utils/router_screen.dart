4465644e9a3f4e0ee80c74fff5bc40f22b980d66
Eslamadelelkhouly <rodyerak@gmail.com>
2025-06-18 19:40:23 +0300

    add router screen

---
+++ lib/core/utils/router_screen.dart
@@ -0,0 +1,15 @@
+import 'package:balancedmeal/features/splash%20screen/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class RouterScreen {
+  static const String splash = '/';
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: splash,
+        builder: (context,state) => const SplashView(),
+      ),
+    ],
+  );
+}
