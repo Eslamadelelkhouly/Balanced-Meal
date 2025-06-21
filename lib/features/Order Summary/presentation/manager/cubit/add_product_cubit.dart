92b50d0dd4d740e4850ae6c55a2155014441933c
Eslamadelelkhouly <rodyerak@gmail.com>
2025-06-21 15:42:18 +0300

    add product cubit

---
+++ lib/features/Order Summary/presentation/manager/cubit/add_product_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:balancedmeal/core/API/api_service.dart';
+import 'package:balancedmeal/features/Home/data/models/product_model.dart';
+import 'package:balancedmeal/features/Order%20Summary/data/repo/add_product_repo_implemenation.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
+
+part 'add_product_state.dart';
+
+class AddProductCubit extends Cubit<AddProductState> {
+  AddProductCubit() : super(AddProductInitial());
+  ApiService apiService = ApiService(dio: Dio());
+  Future<void> addProduct({
+    required List<ProductItem> selectProduct,
+  }) async {
+    emit(AddProductLoading());
+    var result = await AddProductRepoImplemenation(apiService)
+        .addProduct(selectProduct: selectProduct);
+    result.fold(
+      (error) {
+        emit(AddProductFailure(error['error']));
+      },
+      (success) {
+        emit(AddProductSuccess(success));
+      },
+    );
+  }
+}
---
+++ lib/features/Order Summary/presentation/manager/cubit/add_product_state.dart
@@ -0,0 +1,18 @@
+part of 'add_product_cubit.dart';
+
+@immutable
+sealed class AddProductState {}
+
+final class AddProductInitial extends AddProductState {}
+
+final class AddProductLoading extends AddProductState {}
+final class AddProductSuccess extends AddProductState {
+  final Map<String, dynamic> response;
+
+  AddProductSuccess(this.response);
+}
+final class AddProductFailure extends AddProductState {
+  final String error;
+
+  AddProductFailure(this.error);
+}
